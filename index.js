52cbb9ec6c584819d9bae9aafceea08e5dfef3d1
Stephen Tudor <smtudor@gmail.com>
2014-10-08 16:30:04 -0400

    Add support for inlining font files.
    - Strip any query strings or hashes from url filename
    - Add fontPath option
    - Set correct mime type according to font filetype
    
    The only edge case is for SVG files, which can be images or fonts. As of now, the plugin will continue to look for SVG files in the imagePath.

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ function Base64CSS (inputTree, options) {
 
   options = options || {};
   options.imagePath || (options.imagePath = 'public');
+  options.fontPath || (options.fontPath = 'public');
 
   this.inputTree = inputTree;
   this.imagePath = path.join(process.cwd(), options.imagePath);
+  this.fontPath = path.join(process.cwd(), options.fontPath);
   this.extensions = options.extensions || ['css'];
   this.maxFileSize = options.maxFileSize || 4096;
   this.fileTypes = options.fileTypes || ['png', 'jpg', 'jpeg', 'gif', 'svg'];
@@ -21,22 +23,35 @@ module.exports = Base64CSS;
 
 Base64CSS.prototype.processString = function(string) {
   var imagePath = this.imagePath;
+  var fontPath = this.fontPath;
   var maxFileSize = this.maxFileSize;
   var fileTypes = this.fileTypes;
 
   return string.replace(this.urlsRegex, function(match, fileName) {
+    fileName = fileName.replace(/(\?)?(#)(.*)\b/g, '');
     var type = path.extname(fileName).substr(1);
     if (!~fileTypes.indexOf(type)) return match;
 
-    var filePath = path.join(imagePath, fileName);
-    var size = fs.statSync(filePath).size;
+    var prefix = 'image';
+    var filePath;
 
+    if (['ttf', 'otf', 'woff', 'eot'].indexOf(type) !== -1) {
+      prefix = 'application';
+      if (type === 'ttf') type = 'x-font-truetype';
+      if (type === 'otf') type = 'x-font-opentype';
+      if (type === 'woff') type = 'font-woff';
+      if (type === 'eot') type = 'vnd.ms-fontobject';
+      filePath = path.join(fontPath, fileName);
+    } else {
+      if (type === 'jpg') type = 'jpeg';
+      if (type === 'svg') type = 'svg+xml';
+      filePath = path.join(imagePath, fileName);
+    }
+
+    var size = fs.statSync(filePath).size;
     if (size > maxFileSize) return match;
 
-    if (type === 'jpg') type = 'jpeg';
-    if (type === 'svg') type = 'svg+xml';
-
     var base64 = fs.readFileSync(filePath).toString('base64');
-    return 'url("data:image/' + type + ';base64,' + base64 + '")';
+    return 'url("data:' + prefix + '/' + type + ';base64,' + base64 + '")';
   });
 };
